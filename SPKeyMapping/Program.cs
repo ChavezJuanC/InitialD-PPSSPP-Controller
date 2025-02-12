68cc3e1407c76bf60e1712056ae6e8697a594078
JuanChavez <dosomethingwild012712@gmail.com>
2025-02-12 00:18:54 -0600

    Implemented Key Mapping Script

---
+++ SPKeyMapping/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO.Ports;
+
+class Program
+{
+    static string comPort = "dev/ttyACM0";
+    static SerialPort serialPort = new SerialPort(comPort, 9600, Parity.None, 8, StopBits.One);
+
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.Run();
+    }
+
+    private void Run()
+    {
+        serialPort.DataReceived += new SerialDataReceivedEventHandler(spDataReceived);
+        serialPort.Open();
+        Console.Read();
+    }
+
+    private void spDataReceived(object sender, SerialDataReceivedEventArgs e)
+    {
+        Console.WriteLine(serialPort.ReadExisting());
+    }
+}
+
