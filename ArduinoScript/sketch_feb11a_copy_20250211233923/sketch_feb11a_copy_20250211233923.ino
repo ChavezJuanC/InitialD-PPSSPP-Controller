e450403aa5bbad154b3ab2310658b8a3f16a0f56
JuanChavez <dosomethingwild012712@gmail.com>
2025-02-11 23:40:34 -0600

    Batman

---
+++ ArduinoScript/sketch_feb11a_copy_20250211233923/sketch_feb11a_copy_20250211233923.ino
@@ -0,0 +1,41 @@
+int joyPin1 = A0;
+int joyPin2 = A1;
+
+int value1 = 0;
+int value2 = 0;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  value1 = analogRead(joyPin1);
+  delay(100);
+  value2 = analogRead(joyPin2);
+
+  if (value1 < 526) {
+    Serial.println("down");
+  } else if (value1 > 528) {
+    Serial.println("up");
+  }
+
+  Serial.println("");
+
+    if (value2 < 514) {
+    Serial.println("left");
+  }
+  else if (value2 > 516) {
+    Serial.println("right");
+  }
+}
+
+
+/* 
+
+So the idea is to write a C# or python script that monitors a serial port continously.. 
+based on serial port coms, we will turn them into "key strokes"
+
+*/
