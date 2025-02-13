dcf69a2c5de12f9501ad8ad8ef9f5b33d5a126c5
SKRPTZ <43616880+ChavezJuanC@users.noreply.github.com>
2025-02-12 23:46:54 -0600

    Moving from console app to WF

---
+++ ControllerGUI/ControllerGUI/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO.Ports;
+
+using System.Windows.Forms;
+
+namespace ControllerGUI
+{
+    public partial class MainForm : Form
+    {
+        static string comPort = "COM4";
+        static SerialPort serialPort = new SerialPort(comPort, 9600, Parity.None, 8, StopBits.One);
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainFormLoad(object sender, EventArgs e)
+        {
+            StartCollectingSerailPortData();
+        }
+
+        private void StartCollectingSerailPortData()
+        {
+            serialPort.DataReceived += new SerialDataReceivedEventHandler(SPDataRecieved);
+            serialPort.Open();
+        }
+
+        private void SPDataRecieved(object sender, SerialDataReceivedEventArgs e)
+        {
+            Console.WriteLine(serialPort.ReadExisting());
+        }
+    }
+}
